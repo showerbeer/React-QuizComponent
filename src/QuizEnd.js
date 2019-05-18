c8384f3613592425666342a49a0f81b5e56270a6
Christian Lassen <christian@mudbath.com.au>
2019-05-18 19:42:20 +1000

    Module 7 completed

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class QuizEnd extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    handleResetClick() {
+        this.props.resetClickHandler();
+    }
+
     render() {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a href=''>Reset Quiz</a>
+                <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
             </div>
         );
     }
