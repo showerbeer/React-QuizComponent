891363bcaac3133ca7eb2c6d1757b68a38864dea
Christian Lassen <christian@mudbath.com.au>
2019-05-18 16:17:30 +1000

    Completed the module

---
+++ src/QuizQuestion.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {quiz_question} = this.props;
+
+        return (
+            <main>
+                <section>
+                    <p>{quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        {quiz_question.answer_options.map((opt, index) => <li key={index}>{opt}</li>)}
+                    </ul>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default QuizQuestion;
